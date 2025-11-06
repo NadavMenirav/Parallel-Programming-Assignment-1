ef72424cf18b24518227cb3b5688e7ce056f5900
Nadav Menirav <nmenirav@gmail.com>
2025-11-06 22:57:22 +0200

    (Feat) decode function

--- decode.c
+++ decode.c
@@ -1,2 +1,12 @@
 long decode_c_version(long x, long y, long z);
 
+long decode_c_version(long x, long y, long z) {
+    long result = 0;
+    y = y - z;
+    x = x * y;
+    result = z;
+    result = result << 32;
+    result = result >> 32;
+    result = result ^ x;
+    return result;
+}
