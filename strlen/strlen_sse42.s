df4c9eaef0f248787f90e669b615459386d01cff
Nadav Menirav <nmenirav@gmail.com>
2025-11-09 19:09:38 +0200

    (Patch) fixed some % and $

--- strlen/strlen_sse42.s
+++ strlen/strlen_sse42.s
@@ -9,17 +9,17 @@ strlen_sse42:
     pushq %rcx #administrations
     xorl %ecx, %ecx
 
-    pxor xmm0, xmm0 # The null terminator string
-    movdqu (%rdi), xmm1 # the first 16 bytes of our string
+    pxor %xmm0, %xmm0 # The null terminator string
+    movdqu (%rdi), %xmm1 # the first 16 bytes of our string
 
     xorl %eax, %eax # zero the eax register
 
     xorl %esi, %esi #counter
 
 loop: # while we did not reach the end of the string
-    pcmpistri xmm0, xmm1, $0x08 # first index that they are equal
+    pcmpistri %xmm0, %xmm1, $0x08 # first index that they are equal
     add %ecx, %esi # add the number of matches to our counter
-    cmpl 16, %ecx #if we did not reach the null terminator on our string
+    cmpl $16, %ecx #if we did not reach the null terminator on our string
     je loop
     jmp outside
 
