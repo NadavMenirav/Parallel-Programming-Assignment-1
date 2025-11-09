bcd946e89f2c98cbd682629ce5986ae8336416fa
Nadav Menirav <nmenirav@gmail.com>
2025-11-09 19:27:41 +0200

    (Patch) now acutall iterates over the whole string

--- strlen/strlen_sse42.s
+++ strlen/strlen_sse42.s
@@ -10,18 +10,24 @@ strlen_sse42:
     xorl %ecx, %ecx
 
     pxor %xmm0, %xmm0 # The null terminator string
-    movdqu (%rdi), %xmm1 # the first 16 bytes of our string
 
     xorl %eax, %eax # zero the eax register
 
     xorl %esi, %esi #counter
 
 loop: # while we did not reach the end of the string
+    movdqu (%rdi), %xmm1 # the first 16 bytes of our string
     pcmpistri %xmm0, %xmm1, $0x08 # first index that they are equal
+
     add %ecx, %esi # add the number of matches to our counter
     cmpl $16, %ecx #if we did not reach the null terminator on our string
-    je loop
-    jmp outside
+    je add # we need to move te string 16 bytes ahead
+
+    jmp outside #else
+
+add:
+    addq $16, %rdi
+    jmp loop
 
 outside:
     movl %esi, %eax # our return value, number of chars in the string
