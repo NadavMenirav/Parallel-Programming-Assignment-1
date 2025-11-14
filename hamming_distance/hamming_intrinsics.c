7a5f45ab24fe9ad8c1377a1786f79213a38d2463
Nadav Menirav <nmenirav@gmail.com>
2025-11-14 13:10:47 +0200

    (Refactor) comments

--- hamming_distance/hamming_intrinsics.c
+++ hamming_distance/hamming_intrinsics.c
@@ -14,19 +14,33 @@ int hamming_dist(char str1[MAX_STR], char str2[MAX_STR]);
  * all the matches.
  */
 int hamming_dist(char str1[MAX_STR], char str2[MAX_STR]) {
+
+    // Calculating the lengths of the strings
     const size_t len1 = local_strlen(str1);
     const size_t len2 = local_strlen(str2);
 
+    // Storing the string in a char* so that we will be able to +=16 it.
     const char* pstr1 = str1;
     const char* pstr2 = str2;
 
+    // Storing the strings in the xmm registers
     const __m128i firstString = _mm_loadu_si128((__m128i *)pstr1);
     const __m128i secondString = _mm_loadu_si128((__m128i *)pstr2);
 
+
+    /*
+     * The function returns a xmm register that every bit in it represents whether the corresponding chars in
+     * the array are equal
+     */
     const __m128i diff = _mm_cmpeq_epi8(secondString, firstString);
 
+    // The function sets the register 0
     const __m128i zeroString = _mm_setzero_si128();
 
+    /*
+     * The function splits the register in half and sums over it. we use the zero string because it first subtracts it
+     * from our string
+     */
     const __m128i sumOfDiff = _mm_sad_epu8(diff, zeroString);
 
 
