177357a181774f24a5c84e703c3d402d2e5edb94
Nadav Menirav <nmenirav@gmail.com>
2025-11-16 17:36:01 +0200

    (Feat) ds

---
+++ formulas/formula1.c
@@ -0,0 +1,13 @@
+#include <xmmintrin.h> //SSE1
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pmmintrin.h> //SSE3
+
+#define MAX_STR 255
+
+
+// This function will calculate the difficult formula fast, using intrinsics of course.
+float formula1(float *x, unsigned int length) {
+    
+}
